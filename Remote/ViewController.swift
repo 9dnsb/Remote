370a0e11b122533cde0019ecfaa82080db841ca9
David Blat <davidblatt10@gmail.com>
2020-05-05 15:35:56 -0400

    Initial Commit

---
+++ Remote/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Remote
+//
+//  Created by David Blatt on 5/5/20.
+//  Copyright © 2020 David Blatt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
